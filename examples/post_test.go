b3fd3d8c7524cc6775214402e4f624650fb5e29e
userq11 <unlimited5511@interia.pl>
2020-11-03 23:59:36 +0100

    Adding post example

--- examples/post_test.go
+++ examples/post_test.go
@@ -1,20 +1,74 @@
 package examples
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 	"testing"
+
+	"github.com/userq11/go-httpclient/gohttp"
 )
 
-type Repository struct {
-	Name string `json:"name"`
-}
+func TestCreateRepo(t *testing.T) {
+	t.Run("timeoutFromGithub", func(t *testing.T) {
+		gohttp.FlushMocks()
+		gohttp.AddMock(gohttp.Mock{
+			Method:      http.MethodPost,
+			Url:         "https://api.github.com/user/repos",
+			RequestBody: `{"name":"test-repo","private":true}`,
 
-func TestPost(t *testing.T) {
-	repo := Repository{
-		Name: "testing-repo",
-	}
-	response, err := httpClient.Post("https://api.github.com", repo)
+			Error: errors.New("timeout from github"),
+		})
 
-	fmt.Println(err)
-	fmt.Println(response)
+		repository := Repository{
+			Name:    "test-repo",
+			Private: true,
+		}
+
+		repo, err := CreateRepo(repository)
+
+		if repo != nil {
+			t.Error("no repo expected when we get a timeout from github")
+		}
+
+		if err == nil {
+			t.Error("an error is expected when we get a timeout from github")
+		}
+
+		if err.Error() != "timeout from github" {
+			fmt.Println(err.Error())
+			t.Error("invalid error message")
+		}
+	})
+
+	t.Run("noError", func(t *testing.T) {
+		gohttp.FlushMocks()
+		gohttp.AddMock(gohttp.Mock{
+			Method:      http.MethodPost,
+			Url:         "https://api.github.com/user/repos",
+			RequestBody: `{"name":"test-repo","private":true}`,
+
+			ResponseStatusCode: http.StatusCreated,
+			ResponseBody:       `{"id":123, "name":"test-repo"}`,
+		})
+
+		repository := Repository{
+			Name:    "test-repo",
+			Private: true,
+		}
+
+		repo, err := CreateRepo(repository)
+
+		if err != nil {
+			t.Error("no error expected when we get a valid response from github")
+		}
+
+		if repo == nil {
+			t.Error("a valid repo was expected")
+		}
+
+		if repo.Name != repository.Name {
+			t.Error("invalid repository name obtained from github")
+		}
+	})
 }
