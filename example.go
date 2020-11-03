986d8d5d82e581aee3cb47aa9d49e53ef2d9c3b5
userq11 <unlimited5511@interia.pl>
2020-11-03 11:56:10 +0100

    making requests with body

--- example.go
+++ example.go
@@ -24,10 +24,25 @@ func main() {
 	getUrls()
 }
 
-func getUrls() {
-	headers := make(http.Header)
+type User struct {
+	FirstName string `json: "first_name"`
+	LastName  string `json: "last_name"`
+}
 
-	response, err := githubHttpClient.Get("https://api.github.com", headers)
+func getUrls() {
+	response, err := githubHttpClient.Get("https://api.github.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+
+	bytes, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(bytes))
+}
+
+func createUser(user User) {
+	response, err := githubHttpClient.Post("https://api.github.com", nil, user)
 	if err != nil {
 		panic(err)
 	}
